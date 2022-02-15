32419bd0e8dc088c0a02a99eba4c3a7ecb7e3d47
Nickolay Dimitrov <dimitrov.nikolai9224@gmail.com>
2022-02-15 21:58:59 +0200

    Adding the game template structor

--- src/components/game-template/GameTemplate.js
+++ src/components/game-template/GameTemplate.js
@@ -4,14 +4,21 @@ function GameTempalte() {
     return (
         <div className={style.gameTemplateComponentWrapper}>
             <div className={style.gameTempalteWrapper}>
-                <header>
-                    <h1>Game Name</h1>
+                <header className={style.gameTemplateHeader}>
+                    <h1>Game image Slide show</h1>
+                    <img />
                 </header>
                 <main>
+                    <header className={style.mainHeader}>
+                        <p>Icon for OS</p>
+                        <h2>Game Name</h2>
+                    </header>
                     <p>Main Content for the game </p>
                 </main>
                 <footer>
-                    <p>Footer for the game template</p>
+                    <button>Add</button>
+                    <button>Wishlist</button>
+                    <button>Reviews</button>
                 </footer>
             </div>
             <div className={style.gameTempalteWrapper}>
