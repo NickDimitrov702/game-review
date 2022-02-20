02b1aeb15abc15d1851e8a6bbf8f2d98a0b04bf9
Nickolay Dimitrov <dimitrov.nikolai9224@gmail.com>
2022-02-20 17:13:53 +0200

    Adding better styles for sign-up component.

--- src/components/publicRout/PublicRouter.js
+++ src/components/publicRout/PublicRouter.js
@@ -1,7 +1,8 @@
 import { React } from 'react'
 import { Route, Redirect } from 'react-router-dom/cjs/react-router-dom.min'
 import { useAuth } from '../../context/AuthContext'
-// Wrapper for our current rout
+// Wrapper for our current PublicRout, 
+// Pubic rout allows access to Hompepage without registering on the web-app. 
 export default function PublicRout({ component: Component, restricted, ...rest }) {
     const { currentUser } = useAuth()
     return (
--- src/components/sign-up/SignUp.js
+++ src/components/sign-up/SignUp.js
@@ -15,7 +15,7 @@ function SignUp() {
     // Async function since we have try catch, that is using async event within the Singup
     async function handleSubmit(e) {
         e.preventDefault()
-        
+
         // Validations for passwrods
         if (passwordRef.current.value !== passwordConfirmRef.current.value) {
             return setError('Passwords do not match')
@@ -38,27 +38,31 @@ function SignUp() {
 
     return (
 
-            <div className={style.formWrapper}>
-                <form className={style.signUpFormWrapper} onSubmit={handleSubmit}>
-
-                    <label className={style.emailInputWrapper}>Enter email
+        <div className={style.formWrapper}>
+            <form className={style.signUpFormWrapper} onSubmit={handleSubmit}>
+                <div className={style.emailInputWrapper}>
+                    <label className={style.label}>Enter email
                     </label>
                     <input ref={emailRef} className={style.input} type="email"></input>
+                </div>
 
-
-                    <label className={style.passwordInputWrapper}>Enter password
-                        <input ref={passwordRef} className={style.input} type="password" ></input>
+                <div className={style.passwordInputWrapper}>
+                    <label className={style.label}>Enter password
                     </label>
+                    <input ref={passwordRef} className={style.input} type="password" ></input>
 
-
-                    <label className={style.passwordInputWrapper}>Confirm password
-                        <input ref={passwordConfirmRef} className={style.input} type="password"></input>
+                </div>
+                <div className={style.passwordInputWrapper}>
+                    <label className={style.label}>Confirm password
                     </label>
-                    <input type="submit" value="Submit" />
-                    <span><Link to='/log-in'>Already have an account ?</Link></span>
-                    {/* Wont be able to re-submit our form since we are using the loading state, disabled={loading} the btn will be disabled */}
-                </form>
-            </div>
+                    <input ref={passwordConfirmRef} className={style.input} type="password"></input>
+
+                </div>
+                <input className={style.submitBtn} type="submit" value="Submit" />
+                <span className={style.spanLink}><Link to='/log-in'>Already have an account ?</Link></span>
+                {/* Wont be able to re-submit our form since we are using the loading state, disabled={loading} the btn will be disabled */}
+            </form>
+        </div>
 
 
 
